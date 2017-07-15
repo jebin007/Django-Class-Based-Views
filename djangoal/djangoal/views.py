a88e6fb7ec428d144e76d557cfd3f469cfa3d7d2
jebin007 <jevinsubedi@gmail.com>
2017-07-15 18:31:34 -0500

    First View Class:
    -class based view called HelloWorld created.
    -url created for it.
    -pointed to the view as view method in the url.

--- djangoal/djangoal/views.py
+++ djangoal/djangoal/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'home.html')
+
+class HelloWorldView(View):
+    def get(self, request):
+        return HttpResponse("Hello World!")
