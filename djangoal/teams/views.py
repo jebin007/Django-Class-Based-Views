466d58f3922536bf5d4b9043989e9b930600d303
jebin007 <jevinsubedi@gmail.com>
2017-07-16 01:13:46 -0500

    Overriding Methods:
    -created a method in class TeamCreateView which auto selects the logged in coach when creating a new team.
    -crated a method to make sure the coach can only delete their team members and not others.

--- djangoal/teams/views.py
+++ djangoal/teams/views.py
@@ -25,6 +25,12 @@ class TeamCreateView(CreateView):
     model = models.Team
     fields = ("name", "practice_location", "coach")
 
+    def get_initial(self):                              #this function auto selects the logged in coach when creating teams.
+        initial = super().get_initial()
+        initial["coach"] = self.request.user.pk
+        return initial
+
+
 class TeamUpdateView(UpdateView):
     model = models.Team
     fields = ("name", "practice_location", "coach")
@@ -32,5 +38,10 @@ class TeamUpdateView(UpdateView):
 class TeamDeleteView(DeleteView):
     model = models.Team
     success_url = reverse_lazy("teams:list")
+
+    def get_queryset(self):                             #this function makes sure the coach can only delete their team memeber and not others.
+        if not self.request.user.is_superuser:
+            return self.model.objects.filter(coach=self.request.user)
+        return self.model.objects.all()
 
 
